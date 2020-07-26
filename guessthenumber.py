b5af4b25574003bcd9de534b5036e34cc6c870d1
infinitypk <68584821+infinitypk@users.noreply.github.com>
2020-07-26 17:45:29 +0530

    Update guessthenumber.py

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,13 +1,5 @@
-import random as r
 
-r = r.randint(0,21)
 
-guess_num = int(input("guess the number between 1 to 20 : "))
-
-if r <= 20:
-    print("ok")
-else:
-    print("game over")
 import random as r
 user_name = input("what is your name?: ")
 print('Hello {}'.format(user_name))
