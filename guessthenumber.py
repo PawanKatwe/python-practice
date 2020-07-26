dacb340063dc6b8774e5eff827adb5d5a5045037
infinitypk <68584821+infinitypk@users.noreply.github.com>
2020-07-26 17:38:52 +0530

    Update guessthenumber.py

--- guessthenumber.py
+++ guessthenumber.py
@@ -8,3 +8,16 @@ if r <= 20:
     print("ok")
 else:
     print("game over")
+import random as r
+user_name = input("what is your name?: ")
+print('Hello {}'.format(user_name))
+
+r = r.randint(0,11)
+
+win_num = int(input("Guess the number between 1 to 10: "))
+
+if r == win_num:
+	print('Congratulation you win {}'.format(user_name))
+else:
+	print("Game over")
+		
