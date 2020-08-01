d2e31ac721add09ff100bbcdd605bc211c2bfa77
infinitypk <68584821+infinitypk@users.noreply.github.com>
2020-08-01 11:53:02 +0530

    Update rpsgame.py

--- rpsgame.py
+++ rpsgame.py
@@ -1,45 +1,43 @@
-import random
+from random import randint
+
 user_name = input("what is your name?: ")
 print('hello {}'.format(user_name))
-print('Lets play rock paper seasor')
+print('Lets play rock paper scissor')
+print("choose yors from rock,paper or scissor")     
+print("you can say 'quit' to stop game anytime")
 
-user_input = input('choose yours: ').lower()
-'''
-r = str("rock")
-p = str("paper")
-s = str("seasor")
-'''
+t=['rock','paper','scissor']
 
-com_input = ['rock', 'paper', 'seasor']
-print(random.choice(com_input))
 
-while user_input == "rock":
-        if com_input == "rock":
-            print("it is a draw")
-        elif com_input == "paper":
-            print("You loose")
-        elif com_input == "seasor":
-            print("You Win")
-        break
-else: print('thanks for playing')
-    
-while user_input == "seasor":
-        if com_input == "rock":
-            print("You loose")
-        elif com_input == "paper":
-            print("You Win")
-        elif com_input == "seasor":
-            print("It is draw")
-        break
-else: print('thanks for playing')
 
-while user_input == "paper":
-        if com_input == "rock":
-            print("You WIn")
-        elif com_input == "paper":
-            print("It is a draw")
-        elif com_input == "seasor":
-            print("You loose")
-        break
-else: print('thanks for playing')
+
+player_r="false"
+
+while player_r=="false":
+        computer=t[randint(0,2)]
+        player=input("rock,paper,scissor?: ").lower()
+
+        if player==computer:
+                print("Tie!")
+        elif player=="rock":
+                if computer=="paper":
+                        print("You lose!",computer,"covers",player)
+                else:
+                        print("You win!",player,"smashes",computer)
+        elif player=="paper":
+                if computer=="scissor":
+                        print("You lose!",computer,"cut",player)
+                else:
+                        print("You win!",player,"covers",computer)
+        elif player=="scissor":
+                if computer=="Rock":
+                        print("You lose...",computer,"smashes",player)
+                else:
+                        print("You win!",player,"cut",computer)
+        elif player=="quit":
+                print("thanks for playing the game")
+                break
+        else:
+                print("That's not a valid play.Check your spelling!")
+
 
